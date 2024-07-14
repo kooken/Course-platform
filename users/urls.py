9cf5549e1ac8876b9a239037955f15cd1d43be03
mariasazhina <msazhina23@gmail.com>
2024-07-14 14:19:07 +0300

    hw 3.1

---
+++ users/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView, TokenRefreshView, )
+
+from users.apps import UsersConfig
+
+app_name = UsersConfig.name
+
+urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(permission_classes=(AllowAny,)), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(permission_classes=(AllowAny,)), name='token_refresh'),
+    ]
